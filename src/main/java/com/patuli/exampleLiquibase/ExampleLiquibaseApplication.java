a908a21bf9433850af0884ddd16ee1b02d7777cc
pinar-mat <63678861+pinar-mat@users.noreply.github.com>
2020-11-04 06:40:23 +0300

    Initial commit

---
+++ src/main/java/com/patuli/exampleLiquibase/ExampleLiquibaseApplication.java
@@ -0,0 +1,13 @@
+package com.patuli.exampleLiquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleLiquibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleLiquibaseApplication.class, args);
+	}
+
+}
